ff1a6219a616eb603135472478ee2c60e46df78f
Adam Drew <adamrdrew@live.com>
2025-09-20 06:24:54 -0400

    Add new note control

---
+++ NewNoteControl/NewNoteControlBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewNoteControlBundle.swift
+//  NewNoteControl
+//
+//  Created by Adam Drew on 9/20/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewNoteControlBundle: WidgetBundle {
+    var body: some Widget {
+        NewNoteControl()
+    }
+}
